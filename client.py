32e8b2806c44f9681529f41daaba12023536c18a
MathiasDPX <mathias@dupeux.net>
2026-06-30 18:23:31 +0200

    feat: add prometheus exporter

--- client.py
+++ client.py
@@ -2,14 +2,16 @@ import requests
 
 _BASEURL = "http://ws.audioscrobbler.com/2.0/"
 
+
 class Period:
     overall = "overall"
-    
-    def months(x:int):
+
+    def months(x: int):
         return f"{x}month"
-    
-    def days(x:int):
+
+    def days(x: int):
         return f"{x}day"
+
 
 class UserInfo:
     def __init__(self, data):
@@ -18,18 +20,18 @@ class UserInfo:
         self.age = int(user["age"])
         self.realname = user["realname"]
 
-        self.play_count = int(user["playcount"])
-        self.artist_count = int(user["artist_count"])
-        self.track_count = int(user["track_count"])
-        self.album_count = int(user["album_count"])
+        self.play_count = int(user.get("playcount", "0"))
+        self.artist_count = int(user.get("artist_count", "0"))
+        self.track_count = int(user.get("track_count", "0"))
+        self.album_count = int(user.get("album_count", "0"))
 
 
 class TopTrack:
     def __init__(self, data):
-        self.name = data['name']
-        self.artist = data['artist']['name']
-        self.rank = int(data['@attr']['rank'])
-        self.playcount = int(data['playcount'])
+        self.name = data["name"]
+        self.artist = data["artist"]["name"]
+        self.rank = int(data["@attr"]["rank"])
+        self.playcount = int(data.get("playcount", "0"))
 
 
 class LastFMClient:
@@ -38,15 +40,15 @@ class LastFMClient:
         self.apikey = apikey
 
     def _make_request(self, method, extraparams={}):
-        params={
+        params = {
             "method": method,
             "user": self.username,
             "api_key": self.apikey,
-            "format": "json"
+            "format": "json",
         }
         params.update(extraparams)
         r = requests.get(_BASEURL, params=params)
-        
+
         r.raise_for_status()
 
         return r
@@ -56,28 +58,26 @@ class LastFMClient:
         data = r.json()
 
         return UserInfo(data)
-    
+
     def getTopTracks(self, limit=50, page=1, period=Period.overall):
-        params = {
-            "limit": limit,
-            "page": page,
-            "period": period
-        }
+        params = {"limit": limit, "page": page, "period": period}
         r = self._make_request("user.gettoptracks", extraparams=params)
         data = r.json()
-        
-        return [TopTrack(trackdata) for trackdata in data['toptracks']['track']]
+
+        return [TopTrack(trackdata) for trackdata in data["toptracks"]["track"]]
 
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
     from os import getenv
-    
+
     load_dotenv()
 
     client = LastFMClient(getenv("LASTFM_USERNAME"), getenv("LASTFM_APIKEY"))
     info = client.getInfos()
     toptrack = client.getTopTracks(limit=1)[0]
-    
+
     print(f"{info.name} has {info.play_count} scrobbles")
-    print(f"Their most played track is {toptrack.name} by {toptrack.artist} with {toptrack.playcount} scrobbles")
+    print(
+        f"Their most played track is {toptrack.name} by {toptrack.artist} with {toptrack.playcount} scrobbles"
+    )
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from prometheus_client import start_http_server, Gauge
 from dotenv import load_dotenv
+from time import sleep
 from os import getenv
 from client import *
 
@@ -16,5 +18,35 @@ if LASTFM_APIKEY is None:
 
 client = LastFMClient(LASTFM_USERNAME, LASTFM_APIKEY)
 
+playCountGauge = Gauge("play_count", "Number of scrobbles")
+artistCountGauge = Gauge("artist_count", "Number of unique artists scrobbled")
+trackCountGauge = Gauge("track_count", "Number of unique tracks scrobbled")
+albumCountGauge = Gauge("album_count", "Number of unique albums scrobbled")
+trackGauge = Gauge("tracks", "Tracks", ["artist", "name"])
+
 if __name__ == "__main__":
-    pass
+    addr = "0.0.0.0"
+    port = 8000
+    
+    start_http_server(addr=addr, port=port)
+    print(f"Running Prometheus exporter on {addr}:{port}")
+    print(f"exporting {LASTFM_USERNAME}'s metrics")
+    
+    while True:
+        info = client.getInfos()
+        
+        playCountGauge.set(info.play_count)
+        artistCountGauge.set(info.artist_count)
+        trackCountGauge.set(info.track_count)
+        albumCountGauge.set(info.album_count)
+        
+        # 1 <= limit <= 1000
+        toptracks = client.getTopTracks(limit=1000)
+        
+        for track in toptracks:
+            trackGauge.labels(
+                artist=track.artist,
+                name=track.name
+            ).set(track.playcount)
+        
+        sleep(60)
