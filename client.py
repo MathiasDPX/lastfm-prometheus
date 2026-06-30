2dd47863f8a6bd5b83ed0eb01de53f8799552dc2
MathiasDPX <mathias@dupeux.net>
2026-06-30 17:52:16 +0200

    add user.gettoptracks method

--- client.py
+++ client.py
@@ -2,6 +2,14 @@ import requests
 
 _BASEURL = "http://ws.audioscrobbler.com/2.0/"
 
+class Period:
+    overall = "overall"
+    
+    def months(x:int):
+        return f"{x}month"
+    
+    def days(x:int):
+        return f"{x}day"
 
 class UserInfo:
     def __init__(self, data):
@@ -16,21 +24,29 @@ class UserInfo:
         self.album_count = int(user["album_count"])
 
 
+class TopTrack:
+    def __init__(self, data):
+        self.name = data['name']
+        self.artist = data['artist']['name']
+        self.rank = int(data['@attr']['rank'])
+        self.playcount = int(data['playcount'])
+
+
 class LastFMClient:
     def __init__(self, username, apikey):
         self.username = username
         self.apikey = apikey
 
-    def _make_request(self, method):
-        r = requests.get(
-            _BASEURL,
-            params={
-                "method": method,
-                "user": self.username,
-                "api_key": self.apikey,
-                "format": "json"
-            },
-        )
+    def _make_request(self, method, extraparams={}):
+        params={
+            "method": method,
+            "user": self.username,
+            "api_key": self.apikey,
+            "format": "json"
+        }
+        params.update(extraparams)
+        r = requests.get(_BASEURL, params=params)
+        
         r.raise_for_status()
 
         return r
@@ -40,6 +56,17 @@ class LastFMClient:
         data = r.json()
 
         return UserInfo(data)
+    
+    def getTopTracks(self, limit=50, page=1, period=Period.overall):
+        params = {
+            "limit": limit,
+            "page": page,
+            "period": period
+        }
+        r = self._make_request("user.gettoptracks", extraparams=params)
+        data = r.json()
+        
+        return [TopTrack(trackdata) for trackdata in data['toptracks']['track']]
 
 
 if __name__ == "__main__":
@@ -50,5 +77,7 @@ if __name__ == "__main__":
 
     client = LastFMClient(getenv("LASTFM_USERNAME"), getenv("LASTFM_APIKEY"))
     info = client.getInfos()
+    toptrack = client.getTopTracks(limit=1)[0]
     
     print(f"{info.name} has {info.play_count} scrobbles")
+    print(f"Their most played track is {toptrack.name} by {toptrack.artist} with {toptrack.playcount} scrobbles")
--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from dotenv import load_dotenv
 from os import getenv
 from client import *
 
-client = LastFMClient()
+load_dotenv()
+
+LASTFM_USERNAME = getenv("LASTFM_USERNAME")
+LASTFM_APIKEY = getenv("LASTFM_APIKEY")
+
+if LASTFM_USERNAME is None:
+    print("Missing LASTFM_USERNAME environment variable")
+    
+if LASTFM_APIKEY is None:
+    print("Missing LASTFM_APIKEY environment variable")
+
+
+client = LastFMClient(LASTFM_USERNAME, LASTFM_APIKEY)
 
 if __name__ == "__main__":
     pass
